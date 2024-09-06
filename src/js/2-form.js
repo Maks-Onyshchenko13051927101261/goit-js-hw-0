b314208dfc6a9b53a4eccae48459cf6de13f2fc6
Maks Onyshchenko <Onyshchenkomaksymm@gmail.com>
2024-09-07 00:03:27 +0300

    add

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,15 +1,18 @@
-const feetbackFormEl = document.querySelector(".feedback-form");
+const feetbackFormEl = document.querySelector('.feedback-form');
 let formData = {
-  email: "",
-  message: "",
+  email: '',
+  message: '',
 };
 
 const fillFormEl = () => {
-  const formDataLS = JSON.parse(localStorage.getItem("feedback-form-state"));
+  const formDataLS = JSON.parse(localStorage.getItem('feedback-form-state'));
 
   if (formDataLS === null) {
     return;
   }
+
+  formData = formDataLS;
+
   for (const key in formDataLS) {
     if (formDataLS.hasOwnProperty(key)) {
       feetbackFormEl.elements[key].value = formDataLS[key];
@@ -19,23 +22,23 @@ const fillFormEl = () => {
 
 fillFormEl();
 
-const onFormField = (event) => {
+const onFormField = event => {
   const { name, value } = event.target;
   formData[name] = value.trim();
 
-  localStorage.setItem("feedback-form-state", JSON.stringify(formData));
+  localStorage.setItem('feedback-form-state', JSON.stringify(formData));
 };
 
-const onFeedbackSubmit = (event) => {
+const onFeedbackSubmit = event => {
   event.preventDefault();
-  if (formData.email === "" || formData.message === "") {
-    return alert("Fill please all fields");
+  if (formData.email === '' || formData.message === '') {
+    return alert('Fill please all fields');
   }
 
   event.target.reset();
   console.log(formData);
-  localStorage.removeItem("feedback-form-state");
+  localStorage.removeItem('feedback-form-state');
 };
 
-feetbackFormEl.addEventListener("input", onFormField);
-feetbackFormEl.addEventListener("submit", onFeedbackSubmit);
+feetbackFormEl.addEventListener('input', onFormField);
+feetbackFormEl.addEventListener('submit', onFeedbackSubmit);
